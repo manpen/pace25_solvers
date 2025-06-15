9d4c59962a2dfbc204d64116a59e5925ae948985
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2025-06-15 19:55:38 +0200

    Add runner check into first images

--- submissions/pace_yourself/inner_build.sh
+++ submissions/pace_yourself/inner_build.sh
@@ -7,3 +7,6 @@ cargo b --profile optil -F optil
 cp target/optil/{heuristic,exact} /
 cd /
 rm -rf code
+
+# check:
+cd / && /runner run -b /exact -T 10 -G 5 -w "nodes<10"
--- submissions/uzl/inner_build.sh
+++ submissions/uzl/inner_build.sh
@@ -5,3 +5,9 @@ set -x
 . /root/.cargo/env
 cargo b -r
 mv target/release/uzl_ds /exact
+
+cd /
+rm -rf code
+
+# check:
+cd / && /runner run -b /exact -T 10 -G 5 -w "nodes<10"
