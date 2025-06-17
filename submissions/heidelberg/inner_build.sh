e96322760eebc68ce2fce39ac7004a2b21958299
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2025-06-17 11:53:57 +0200

    Heidelberg add wrapper script in order not to move binaries

--- submissions/heidelberg/inner_build.sh
+++ submissions/heidelberg/inner_build.sh
@@ -13,6 +13,18 @@ perl -pi -e 's/git\@github.com:/https:\/\/github.com\//' *.sh
 ./get_dep.sh
 make -j
 
-mv EXACT /exact
+#mv EXACT /exact
+#mv HEURISTIC /heuristic
 
-cd / && /runner run -b /exact -T 10 -G 5 -w "nodes<10"
+echo '#!/bin/sh' > /exact
+echo 'cd /code' >> /exact
+echo '/code/EXACT' >> /exact
+
+echo '#!/bin/sh' > /heuristic
+echo 'cd /code' >> /exact
+echo '/code/HEURISTIC' >> /heuristic
+
+chmod +x /exact /heuristic
+
+cd /
+/runner run -b /exact -T 10 -G 5 -w "nodes<10"
